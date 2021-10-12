9560abad09b072e167cc26cd7a5fffc733300699
Jamontoast24 <65861880+Jamontoast24@users.noreply.github.com>
2021-10-12 13:28:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
 //Create the actual calculate app
-    //Create Button variable
-    // let button = document.querySelectorAll('.btn')
-    // //Create a button that gives us the total of what we entered
-    // let resultEqual = document.querySelector('.operatorBtnEqual')
-    // //Create a button that deletes our current input
-    // let clearCalc = document.getElementById('operatorBtnDelete')
-    // //Create a screen for our calculator
-    // let calcScreen = document.querySelector('.displayScreen')
-    // //Create a variable to target our HTML form
-    // let screenForm = document.querySelector('.calculatorScreen')
 const calculatorApp = () => {
 
     //First define all the variables we'll need to create the calculator
@@ -52,10 +42,6 @@ const calculatorApp = () => {
     //Create a function that calculates what the user has input and make sure the user has input something or throw an error message
     const displayResult = () => {
         resultEqual.addEventListener('click', function(e) {
-            //Create a variable that stores the result
-            // let result = eval(calcScreen.value)
-            // calcScreen.value = result
-
             //Create a message to throw at user if no input has been detected
             if (calcScreen.value === '') {
                 calcScreen.value = 'Error : Enter a value you would like to calculate'
@@ -70,34 +56,3 @@ const calculatorApp = () => {
 
 }
 calculatorApp()
-
-
-// (function(){
-  
-//     let screen = document.querySelector('.displayScreen');
-//     let buttons = document.querySelectorAll('.btn');
-//     let clear = document.getElementById('operatorBtnDelete');
-//     let equal = document.querySelector('.operatorBtnEqual');
-    
-//     //retrieve data from numbers that are clicked
-//     buttons.forEach(function(button){
-//       button.addEventListener('click', function(e){
-//         let value = e.target.dataset.num;
-//         screen.value += value;
-//       })
-//     });
-    
-//     equal.addEventListener('click', function(e){
-//       if(screen.value === ''){
-//         screen.value = 'Please Enter a Value';
-//       } else {
-//         let answer = eval(screen.value);
-//         screen.value = answer;
-//       }
-//     })
-    
-//     clear.addEventListener('click', function(e){
-//       screen.value = '';
-//     })
-   
-//   })(); 
